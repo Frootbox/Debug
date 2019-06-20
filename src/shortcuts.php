69736fa8927eab5bb27caea918e23d53ebfce275
Jan Habbo Brüning <jan.habbo.bruening@gmail.com>
2019-06-20 07:27:48 +0200

    improved debug shortcuts

--- src/shortcuts.php
+++ src/shortcuts.php
@@ -7,8 +7,32 @@
 
 function d ( $input ) {
     
-    echo "<pre>";
-    print_r($input);
-    echo "</pre>";
+    while (ob_get_level() > 0) {
+        ob_end_clean();
+    }
+    
+    p($input);
+    
     exit;
+}
+
+function p ( $input ) {
+    
+    echo "<pre>";
+    
+    ob_start();
+    
+    print_r($input);
+    
+    $input = ob_get_contents();
+    
+    ob_end_clean();
+    
+    $input = str_replace('>', '&gt;', $input);
+    $input = str_replace('<', '&lt;', $input);
+    
+    
+    print_r($input);
+    
+    echo "</pre>";
 }
